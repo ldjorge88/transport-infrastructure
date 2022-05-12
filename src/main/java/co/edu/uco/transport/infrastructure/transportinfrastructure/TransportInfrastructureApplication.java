03a2bf7941934388e63dc79f80b0dfd83e6bee90
ldjorge1988@gmail.com <ldjorge1988@gmail.com>
2022-05-11 22:36:15 -0500

    creacion capa de insfraestructura

---
+++ src/main/java/co/edu/uco/transport/infrastructure/transportinfrastructure/TransportInfrastructureApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uco.transport.infrastructure.transportinfrastructure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransportInfrastructureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransportInfrastructureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/uco/transport/infrastructure/transportinfrastructure/TransportInfrastructureApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uco.transport.infrastructure.transportinfrastructure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransportInfrastructureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
